369a68a7954f6a6d00f48f6a12da125faf3f43c1
Mohammad Amanour Rahman <126825646+aman0311x@users.noreply.github.com>
2024-08-09 03:30:55 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ import 'fontstyle.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
